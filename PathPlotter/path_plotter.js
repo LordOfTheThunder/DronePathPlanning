8413d54edc084d470375dffbbf54348f1575d238
Max Kolchinsky <max.kolchinsky@gmail.com>
2019-04-24 15:31:57 +0300

    Added path plotter in JS

---
+++ PathPlotter/path_plotter.js
@@ -0,0 +1,187 @@
+var fps = 2, fpsInterval, startTime, now, then, elapsed;
+var width, height;
+var translated_start_point, translated_path_coordinates, translated_coordinates;
+var draw_point_width = 4;
+var draw_pos = 0;
+
+function init() {
+	// Init canvas
+	initCanvas();
+	// Translate coordinates to our screen sizeToContent
+	translateCoordinates();
+	// Draw start point - path1.start_point
+	drawStartPoint();
+	// Draw sensors with radius - path1.sensors
+	drawSensors();
+	// Draw path animation - path1.path
+	startAnimating(fps);
+}
+
+function initCanvas() {
+	var c = document.getElementById("myCanvas");
+	width = document.body.clientWidth;
+	height = document.body.clientHeight;
+	c.width = width;
+	c.height = height;
+}
+
+function translateCoordinates() {
+	// start_point holds the start point (2d point)
+	// sensor_points hold the locations of the sensors + their radii (3d point)
+	var full_points = start_point.concat(" ");
+	full_points = full_points.concat(sensor_points);
+	points_list = full_points.split(" ");
+	
+	// path_points hold the locations of the path (2d point)
+	var full_path_points = start_point.concat(" ");
+	full_path_points = full_path_points.concat(sensor_points);
+	full_path_points = full_path_points.concat(" ");
+	full_path_points = full_path_points.concat(start_point);
+	path_list = full_path_points.split(" ");
+	
+	function findLimitCoordinates() {
+		var rightmost = 0, leftmost = width, bottommost = height, topmost = 0;
+		for (var i = 0; i < points_list.length; i++) {
+			var point = points_list[i];
+			var splitted_point = point.split(",");
+			var x = parseInt(splitted_point[0], 10);
+			var y = parseInt(splitted_point[1], 10);
+			var radius = parseInt(splitted_point[2], 10);
+			rightmost = Math.max(rightmost, x + radius);
+			leftmost = Math.min(leftmost, x - radius);
+			topmost = Math.max(topmost, y + radius);
+			bottommost = Math.min(bottommost, y - radius);
+		}
+		return [rightmost, leftmost, topmost, bottommost];
+	}
+	
+	function translateFromLimits(coords, points_list) {
+		var rightmost = coords[0], leftmost = coords[1], topmost = coords[2], bottommost = coords[3];
+		var translated_points = [];
+		for (var i = 0; i < points_list.length; i++) {
+			var point = points_list[i];
+			var splitted_point = point.split(",");
+			var x = parseInt(splitted_point[0], 10);
+			var y = parseInt(splitted_point[1], 10);
+			var new_x = (x - leftmost) / (rightmost - leftmost) * width;
+			var new_y = (y - bottommost) / (topmost - bottommost) * height;
+			if (splitted_point.length == 3) {
+				translated_points.push([new_x, new_y, splitted_point[2]]);
+			} else {
+				translated_points.push([new_x, new_y]);
+			}
+		}
+		return translated_points;
+	}
+	var coords = findLimitCoordinates();
+	// First point returned is the start point
+	translated_coordinates = translateFromLimits(coords, points_list);
+	// Calculate for path_list
+	translated_path_coordinates = translateFromLimits(coords, path_list);
+}
+
+function drawSensors() {
+  var c = document.getElementById("myCanvas");
+  var ctx = c.getContext("2d");
+  
+  for (var i = 1; i < translated_coordinates.length; i++) {
+	var translated_coordinate = translated_coordinates[i];
+	
+	ctx.save();
+  
+	// Draw the sensors
+	ctx.beginPath();
+	ctx.fillStyle = "#99F929"
+	ctx.arc(translated_coordinate[0], translated_coordinate[1], draw_point_width, 0, Math.PI * 2, true);
+	ctx.fill();
+
+	ctx.restore();
+	
+	// Draw sensor radius
+	ctx.save();
+	
+	ctx.beginPath();
+	ctx.strokeStyle = "#FF0101"
+	ctx.arc(translated_coordinate[0], translated_coordinate[1], translated_coordinate[2], 0, Math.PI * 2, true);
+	ctx.stroke();
+	
+	ctx.restore();
+  }
+}
+
+function drawStartPoint() {
+  var c = document.getElementById("myCanvas");
+  var ctx = c.getContext("2d");
+  
+  translated_start_point = translated_coordinates[0];
+  ctx.save();
+  
+  ctx.beginPath();
+  ctx.fillStyle = "#1034C4"
+  ctx.arc(translated_start_point[0], translated_start_point[1], draw_point_width, 0, Math.PI * 2, true);
+  ctx.fill();
+  
+  ctx.restore();
+}
+
+function drawPath() {
+  var c = document.getElementById("myCanvas");
+  var ctx = c.getContext("2d");
+  
+  ctx.save();
+  
+  ctx.beginPath();
+  ctx.strokeStyle = "#5F0505"
+  var curr_cord = translated_path_coordinates[draw_pos];
+  var next_cord = translated_path_coordinates[++draw_pos];
+  var curr_x = curr_cord[0];
+  var curr_y = curr_cord[1];
+  var next_x = next_cord[0];
+  var next_y = next_cord[1];
+  ctx.moveTo(curr_x, curr_y);
+  ctx.lineTo(next_x, next_y);
+  ctx.stroke();
+  
+  ctx.restore();
+}
+
+// initialize the timer variables and start the animation
+
+function startAnimating(fps) {
+    fpsInterval = 1000 / fps;
+    then = Date.now();
+    startTime = then;
+    animate();
+}
+
+// the animation loop calculates time elapsed since the last loop
+// and only draws if your specified fps interval is achieved
+
+function animate() {
+	// request another frame
+
+    var myReq = requestAnimationFrame(animate);
+
+    // calc elapsed time since last loop
+
+    now = Date.now();
+    elapsed = now - then;
+
+    // if enough time has elapsed, draw the next frame
+
+    if (elapsed > fpsInterval) {
+
+        // Get ready for next frame by setting then=now, but also adjust for your
+        // specified fpsInterval not being a multiple of RAF's interval (16.7ms)
+        then = now - (elapsed % fpsInterval);
+
+        // Put your drawing code here
+		if (draw_pos == translated_path_coordinates.length - 1) {
+			cancelAnimationFrame(myReq);
+		}
+		
+		drawPath();
+    }
+}
+
+init();
